32cd1c69e567e2991b9147f86730444fec117aee
Rui Lebre <ruilebre@ua.pt>
2021-03-12 15:10:32 +0000

    Documents format test

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,26 +2,25 @@ const Comment = require('../models/post-comment');
 
 const { normalizeErrors } = require('../helpers/mongoose');
 
-
 exports.likeComment = (req, res) => {
-    const commentId = req.params.id;
+  const commentId = req.params.id;
 
-    if (!commentId) {
-        return res.status(422).send({ errors: [{ title: "Data missing", detail: "Provide Comment ID" }] });
-    }
+  if (!commentId) {
+    return res.status(422).send({ errors: [{ title: 'Data missing', detail: 'Provide Comment ID' }] });
+  }
 
-    Comment.findByIdAndUpdate(commentId, { $inc: { 'likes': 1 } })
-        .select('_id comment likes createdAt')
-        .exec((err, comment) => {
-            if (err) {
-                return res.status(422).send({ errors: normalizeErrors(err.errors) });
-            }
+  Comment.findByIdAndUpdate(commentId, { $inc: { 'likes': 1 } })
+    .select('_id comment likes createdAt')
+    .exec((err, comment) => {
+      if (err) {
+        return res.status(422).send({ errors: normalizeErrors(err.errors) });
+      }
 
-            if (!comment) {
-                return res.status(422).send({ errors: [{ title: 'Invalid Comment ID!', detail: 'Comment does not exist.' }] });
-            }
+      if (!comment) {
+        return res.status(422).send({ errors: [{ title: 'Invalid Comment ID!', detail: 'Comment does not exist.' }] });
+      }
 
-            comment.likes++;
-            res.json(comment);
-        });
+      comment.likes++;
+      res.json(comment);
+    });
 };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const paginate = require('express-paginate');
-const config = require("./config");
+const config = require('./config');
 
 const postRoutes = require('./routes/post');
 const imageUploadRoutes = require('./routes/images');
@@ -13,27 +13,33 @@ const commentsRoutes = require('./routes/comments');
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${config.DB_USER}:${config.DB_SECRET}@${config.DB_HOST}/${config.DB_NAME}?retryWrites=true&w=majority`, {
+mongoose.connect(
+  `mongodb+srv://${config.DB_USER}:${config.DB_SECRET}@${config.DB_HOST}/${config.DB_NAME}?retryWrites=true&w=majority`,
+  {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-});
+  }
+);
 
 //db.posts.createIndex( { "title": "text", "details": "text", "county": "text", "stories":"text", "suggestedFunctions": "text", "comments": "text"} )
 
-
-var allowlist = ['http://localhost:8080', 'https://patrimonioesquecido.ruilebre.com', 'https://lostheritage.ruilebre.com'];
+var allowlist = [
+  'http://localhost:8080',
+  'https://patrimonioesquecido.ruilebre.com',
+  'https://lostheritage.ruilebre.com'
+];
 
 var corsOptionsDelegate = function (req, callback) {
-    var corsOptions;
-    if (allowlist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-    } else {
-        corsOptions = { origin: false } // disable CORS for this request
-    }
-    callback(null, corsOptions) // callback expects two parameters: error and options
-}
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 app.use(cors(corsOptionsDelegate));
 app.use(cors());
@@ -47,5 +53,5 @@ app.use('/api/v1/infos', infoRoutes);
 app.use('/api/v1/comments', commentsRoutes);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Server running");
+  console.log('Server running');
 });
